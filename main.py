672a26038ad746887ee61e9ec939d284c24c9a30
Mehmet Hazar Tok <sukranmt1967@gmail.com>
2021-10-20 15:48:48 +0300

    main

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ n=[50,51,52,53,54,
    65]
 print(n)
 
-for veri in a:
-    for veri2 in b:
-        for veri3 in c:
-            for eleman in n:
-                eleman == ((6 * veri) + (9 * veri2) + (20 * veri3))
-                veri == eleman - ((9 * veri2) + (20 * veri3))
-                veri2 == eleman - ((6 * veri) + (20 * veri3))
-                veri3 == eleman - ((6 * veri) + (9 * veri2))
-                n.sort()
-                if eleman == ((6 * veri) + (9 * veri2) + (20 * veri3)):
-                    print("A =",veri,",",
-                          "B =",veri2,",",
-                          "C =",veri3," için",
-                          "n = ",eleman)
+for eleman in n:
+    for veri in a:
+        for veri2 in b:
+            for veri3 in c:
+
+                    eleman == ((6 * veri) + (9 * veri2) + (20 * veri3))
+                    veri == eleman - ((9 * veri2) + (20 * veri3))
+                    veri2 == eleman - ((6 * veri) + (20 * veri3))
+                    veri3 == eleman - ((6 * veri) + (9 * veri2))
+                    n.sort()
+                    if eleman == ((6 * veri) + (9 * veri2) + (20 * veri3)):
+                        print("n = ",eleman,"için",
+                              "A =",veri,",",
+                              "B =",veri2,",",
+                              "C =",veri3)
 
